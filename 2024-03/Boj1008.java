01b97ffa92133b5fbd1e4b9c42dff83a4f88e2ce
jyPark4321 <qkrwnddus93@gmail.com>
2024-03-12 17:27:01 +0900

    BOJ 1008번

---
+++ 2024-03/Boj1008.java
@@ -0,0 +1,15 @@
+package main.java.AlStudy;
+import java.util.*;
+
+public class Boj1008 {
+
+	public static void main(String[] args) {
+		//A와 B를 입력받은다음 A/B를 출력하는 프로그램
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		System.out.println(a/b);
+
+	}
+
+}
